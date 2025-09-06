243e6e6c504ccc6dc5cf623f142ec31a21d21ddc
Vivek Kadam <kadamvivek5708@gmail.com>
2025-09-06 12:03:11 +0530

    currency converter

---
+++ 06_currency_converter/src/components/index.js
@@ -0,0 +1,5 @@
+// This js is made for importing all components and exporting from this folder so that when we need all components we just import this one single file
+
+import InputBox from "./InputBox";
+
+export {InputBox}
