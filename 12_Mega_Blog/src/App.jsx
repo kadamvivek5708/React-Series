58b8cd6b74f0dbdb0932ebabcc137bb5dbe9524f
Vivek Kadam <kadamvivek5708@gmail.com>
2025-09-19 16:37:51 +0530

    redux, store, tailwind

--- 12_Mega_Blog/src/App.jsx
+++ 12_Mega_Blog/src/App.jsx
@@ -1,11 +1,35 @@
-import conf from "./conf/conf"
+import React,{useState,useEffect} from "react";
+import {useSelector,useDispatch} from 'react-redux';
+import authService from "./appwrite/auth";
+import {login,logout} from "./store/authSlice"
+import {Header,Footer} from "./components/index"
+
 function App() {
-  console.log(conf.appwriteBucketId)
-  return (
-    <>
-      <div>This is Blog App with appwrite</div>
-    </>
-  )
+  const [loading,setLoading] = useState(true);
+  const dispatch =  useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+        .then((userData) => {
+        if (userData) {
+          dispatch(login(userData))
+        }
+        else{
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  },[]);
+
+  return !loading ? (<div className="min-h-screen flex flex-wrap content-between bg-gray-200">
+    <div className="w-full block">
+      <Header/>
+      <main>
+        {/* <Outlet/> */}
+      </main>
+      <Footer/>
+    </div>
+  </div>) : null
 }
 
 export default App
